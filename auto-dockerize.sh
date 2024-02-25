a42791be3af518d16d03b84811719386e321ff7d
Sulav-Adhikari <sulav.adk.7@gmail.com>
2024-02-26 02:47:43 +0545

    Updated the volumes

--- auto-dockerize.sh
+++ auto-dockerize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-LOG_FILE="auto-start-script.log"
+#LOG_FILE="auto-start-script.log"
 
-exec &>> "$LOG_FILE"
+#exec &>> "$LOG_FILE"
 
 #Build images
 docker build -t database_image ./database
@@ -15,16 +15,10 @@ docker build -t frontend_image ./frontend
 #Create network
 docker network create network-B-D
 
-#create volume
-docker volume create database_volume
-
-docker volume create backend_volume
-
-docker volume create frontend_volume
 
 #Run containers
-docker run --name pdatabase_container -itd -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=123456 -p 5433:5432 --network network-B-D -v database_volume:/var/lib/postgressql/data database_image
+docker run --name pdatabase_container -itd -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=123456 -p 5433:5432 --network network-B-D database_image
 
-docker run -itd --name backend_container  --network network-B-D -p 3000:3000 -v backend_volume:/app backend_imagee
+docker run -itd --name backend_container  --network network-B-D -p 3000:3000 backend_imagee
 
-docker run -itd --name frontend_container -p 5173:5173 -v frontend_volume:/app frontend_image
+docker run -itd --name frontend_container -p 5173:5173 frontend_image
